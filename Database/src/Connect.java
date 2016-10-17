7de6f7f556ec2a7724f0d388cda3372a5ac5256b
Brando Guevarra <brandoguevarra@hotmail.com>
2016-10-16 19:53:49 -0700

    first commit

---
+++ Database/src/Connect.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+     
+public class Connect {
+
+	public static void main(String args[]) {
+		  
+		String url = "jdbc:oracle:thin:@localhost:1522:ug";
+		String user = "ora_k5o0b";
+		String password = "a54223152";
+		Connection con;
+		
+		Statement stmt;
+		String query = "select AU_FNAME from AUTHORS";
+	
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+	
+		} catch(java.lang.ClassNotFoundException e) {
+			System.err.print("ClassNotFoundException: ");
+			System.err.println(e.getMessage());
+		}	
+	
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			stmt = con.createStatement();							
+
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String s = rs.getString("AU_FNAME");
+				System.out.println(s);
+			}
+			
+			stmt.close();
+			con.close();
+	
+		} catch(SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+		}
+	}
+}
